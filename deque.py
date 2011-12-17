0c607f387ce24892ccd7e6cdf9abf820c0989fc6
ciferkey <ciferkey@gmail.com>
2011-12-17 00:26:00 -0500

    first commit

---
+++ deque.py
@@ -0,0 +1,28 @@
+import libtorrent as lt
+import time
+
+ses = lt.session()
+ses.listen_on(6881, 6891)
+params = {
+    'save_path': '',
+    'storage_mode': lt.storage_mode_t(2),
+    'paused': False,
+     'auto_managed': True,
+    'duplicate_is_error': True
+    }
+link = "magnet:?xt=urn:btih:04C0FBA9E272E086C47C225A416A06C85FC70810&dn=Bleach%20Season%201-11%20%2b%20movies&tr=http%3a//tracker.thepiratebay.org/announce"
+handle = lt.add_magnet_uri(ses, link, params)
+ses.start_dht()
+
+print 'downloading metadata...'
+while (not handle.has_metadata()):
+        time.sleep(1)
+        print 'got metadata, starting torrent download...'
+        while (handle.status().state != lt.torrent_status.seeding):
+                s = handle.status()
+                state_str = ['queued', 'checking', 'downloading metadata', \
+                                        'downloading', 'finished', 'seeding', 'allocating']
+                    print '%.2f%% complete (down: %.1f kb/s up: %.1f kB/s peers: %d) %s %.3' % \
+                                        (s.progress * 100, s.download_rate / 1000, s.upload_rate / 1000, \
+                                                s.num_peers, state_str[s.state], s.total_download/1000000)
+                                    time.sleep(5)
