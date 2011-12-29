9473eb51c9f639d19cafd3848f73fe85fd857f21
ciferkey <ciferkey@gmail.com>
2011-12-29 00:45:28 -0500

    moved to link file

--- deque.py
+++ deque.py
@@ -1,7 +1,11 @@
-import stream
+#import stream
 import torrent
 
+links = open('links')
+
 d = torrent.Downloader()
-h = d.add_from_ml(r'magnet:?xt=urn:btih:c3b72f56e1ca81f16170b828e94fd0c0f2938707&dn=Bleach+Anime+-+All+Episodes+%26+Movies+-+English+Subs&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80&tr=udp%3A%2F%2Ftracker.ccc.de%3A80e')
+h = d.add_from_ml(links.read())
 d.mark_for_download(h, 'Bleach Anime/Bleach/Bleach 001.avi')
 d.run_until_complete(h)
+
+links.close()
